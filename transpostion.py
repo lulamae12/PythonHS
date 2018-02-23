e6cf2235508f0031839022234def2c9211b3062c
Tommy Smith <tls.tommy11@gmail.com>
2018-02-23 08:10:42 -0600

    made decryption

--- transpostion.py
+++ transpostion.py
@@ -10,9 +10,11 @@ def menu():
     mode = int(input())
     if mode == 2:
         encrypt()
+    if mode == 1:
+        decrypt()
 def encrypt():
     key = int(input("Please enter your key:"))
-    message = str(input("Please enter your message to encrypted:"))
+    message = str(input("Please enter your message to be encrypted:"))
     encryptls = []
     biglist = []
     messagelength = len(message)
@@ -31,4 +33,21 @@ def encrypt():
         encryptls.append("".join(i[num] for i in biglist))
 
     print("".join(encryptls))
+def decrypt():
+    key = int(input("Please enter your key:"))
+    message = str(input("Please enter your message to be decrypted:"))
+    decryptls = []
+    biglist = []
+    messagelength = len(message)
+    amounttorun = ceil(messagelength / key)
+    ATRint = int(amounttorun)
+    counter = 0
+    print(message)
+    for i in range(0,amounttorun):
+        for char in message[i::amounttorun]:
+            decryptls.append(char)
+        decryptls = [i for i in decryptls]
+    print(decryptls)
+
+
 menu()
