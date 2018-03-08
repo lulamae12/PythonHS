43afdd7655b561aafc5a7e34e145a6692db39636
Tommy Smith <tls.tommy11@gmail.com>
2018-03-08 08:13:27 -0600

    made

---
+++ dictionary.py
@@ -0,0 +1,5 @@
+dictionary = {'apple: a red fruit', 'banana: a yellow fruit', 'orange: a orange fruit'}
+wordchoice = input('enter a word to look up:')
+word = (dictionary[key])
+if wordchoice is word:
+    print (word)
