a1706fe6b8e71f6ff49c84c00b02ec7594fdb9ef
lulamae12 <tls.tommy11@gmail.com>
2018-06-10 10:20:19 -0500

    made

--- FortniteDropPicker.py
+++ FortniteDropPicker.py
@@ -1,8 +1,10 @@
 import random
-places = ['Junk Junction', 'Anarachy Acres', 'Risky Reels', 'Haunted Hills', 'Pleasent Park', 'Loot Lake', 'Tomato Town', 'Anarachy Acres', 'Wailing Woods', 'Snobby Shores', 'Tilted Towers', 'Dusty Divot', 'Retail Row', 'Lonely Lodge', 'Greasy Grove', 'Shifty Shafts', 'Salty Springs', 'Flush Factory', 'Fatal Fields',
+places = ['Junk Junction', 'Anarachy Acres', 'Risky Reels', 'Haunted Hills', 'Pleasent Park', 'Loot Lake', 'Tomato Town', 'Wailing Woods', 'Snobby Shores', 'Tilted Towers', 'Dusty Divot', 'Retail Row', 'Lonely Lodge', 'Greasy Grove', 'Shifty Shafts', 'Salty Springs', 'Flush Factory', 'Fatal Fields',
  'Lucky Landing', 'Moistry Mire']
 gridLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
-letter = random.choice(gridLetters)
-place = random.choice(places)
-print("drop at: " + letter + "," + str(random.randint(1,10)))
-print("or, " + place)
+while True:
+    redo = input("hit ENTER to find a place to drop.")
+    letter = random.choice(gridLetters)
+    place = random.choice(places)
+    #print("\ndrop at: " + letter + "," + str(random.randint(1,10)))
+    print("or, " + place +"\n")
