9cef78db3bb6863c0281f537682af811573d4398
Tommy Smith <tls.tommy11@gmail.com>
2019-09-25 07:55:36 -0500

    staged

--- netstat.py
+++ netstat.py
@@ -1,7 +1,9 @@
 import speedtest
 from wifi import Cell, Scheme
-import subprocess
+import subprocess,socket,sys
 import os
+from datetime import datetime
+
 cls = lambda: os.system('cls')
 
 st = speedtest.Speedtest()
@@ -17,14 +19,49 @@ def downloadSpeed():
     download = (download / 1000000)
     return download
 
+def checkPorts():
+    cls()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remoteServer = input("Enter a remote host to scan: ")
+    remoteServerIP  = socket.gethostbyname(remoteServer)
+    print("============================================")
+    print("Scanning server, ",remoteServerIP)
+    print("============================================")
+    timeStart = datetime.now()
+    
+    for port in range(23,1025):
+        result = sock.connect_ex((remoteServer,int(port)))
+        if result == 0:
+            print("Port ",port," is open")
+    timeFinish = datetime.now()
+    totalTime = timeFinish - timeStart
+    print("finished in ", totalTime)
+def checkPort(port):
 
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    
+    result = sock.connect_ex(('76.22.229.4',int(port)))
+    if result == 0:
+        print("Port is open")
+    else:
+        print ("Port is not open")
+    sock.close()
 
 while True:
+    portQues = input("port: ")
+
+    checkPort(portQues)
+checkPorts()
+
+while False:
 
     upload = uploadSpeed()
     download = downloadSpeed()
-    print('==================================================')
-    print('==== download: '+ str(download) +'===')
-    print('==== upload: '+ upload) +'===')
-    print('==================================================')
     cls()
+    print('==================================================')
+    print('==== download: '+ str(download) + '===')
+    print('==== upload: '+str(upload) +'===')
+    print('====')
+    print('==================================================')
+    #cls()
---
+++ pepegaVar.py
@@ -0,0 +1,8 @@
+varaibleName = "Value"
+#variable name = value
+
+3 = 0
+
+2 = 1
+
+print(3 + 2)
